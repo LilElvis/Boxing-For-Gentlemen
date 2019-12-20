2166f0df5331ae66356416d434791b957f1447f2
Ray <raymond.dee@ontariotechu.net>
2019-12-19 20:12:32 -0500

    [ADD/MOD] Created .gitignore. Removed files to re-filter through .gitignore.

--- FizzleDemo/copydeps.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem copy dependencies by D. Buckstein
-set platform=%1
-set config=%2
-set src="%~dp0\ThirdParty\lib\OpenAL\%platform%\%config%\OpenAL32.dll"
-set dst="%~dp0\bin\%platform%\%config%"
-xcopy %src% %dst% /y
